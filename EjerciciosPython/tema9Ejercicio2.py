63d066527a7211e58c8024700806cfd11980c3c9
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-08-14 20:31:14 -0300

    tema9Ejercicio2b

--- EjerciciosPython/tema9Ejercicio2.py
+++ EjerciciosPython/tema9Ejercicio2.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def buscarImpares(lista):
     for i in range(lista):
         if lista%2==0:
@@ -7,4 +10,8 @@ def buscarImpares(lista):
     
 lista=[1,2,3,4,5,6,7,8,9]
 
-print(f"los números imapres de la lista son: {list(filter(buscarImpares,lista))}")
+impares=list(filter(buscarImpares,lista))
+suma=reduce(lambda x,y: x+y,impares)
+
+print(f"los números imapres de la lista son: {impares}")
+print(f"la suma de los números impares es: {suma}")
