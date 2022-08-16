e6922882a5c0398218c2e4d9bbbe69c5871e8dcc
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-08-16 17:00:16 -0300

    tema10Ejercicio2b

--- EjerciciosPython/tema10Ejercicio2.py
+++ EjerciciosPython/tema10Ejercicio2.py
@@ -8,6 +8,6 @@ window = tk.Tk()
 tk.Label(window, text="Seleccionar color:", bg="black", fg="white").pack(side="top", fill="both", ipadx=50, ipady=50)
 
 coloresControlado=tk.StringVar(window, colores)
-tk.Listbox(window, listvariable=coloresControlado).pack(side="bottom", fill="both", padx=50, pady=50)
+tk.Listbox(window, listvariable=coloresControlado).pack(side="bottom", fill="both", padx=25, pady=25)
 
 window.mainloop()
