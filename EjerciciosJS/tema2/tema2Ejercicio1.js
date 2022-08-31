5e4261d002fd0b5a0965d165a18f9044b3237881
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-08-31 18:06:50 -0300

    aux

--- EjerciciosJS/tema2/tema2Ejercicio1.js
+++ EjerciciosJS/tema2/tema2Ejercicio1.js
@@ -1,12 +1,12 @@
-const fecha_nacimiento =  new Date(1999,2,15)
+const fecha_nacimiento = new Date(1999, 2, 15);
 
 const libro_favorito = {
-    titulo: "Así habló Zaratustra",
-    autor: "Nietzche",
-    fecha: 1885,
-    url: "https://es.wikipedia.org/wiki/As%C3%AD_habl%C3%B3_Zaratustra",
-}
+  titulo: "Así habló Zaratustra",
+  autor: "Nietzche",
+  fecha: 1885,
+  url: "https://es.wikipedia.org/wiki/As%C3%AD_habl%C3%B3_Zaratustra",
+};
 
-const lista = ["Gabriel Barimboim", 23, true, fecha_nacimiento, libro_favorito]
+const lista = ["Gabriel Barimboim", 23, true, fecha_nacimiento, libro_favorito];
 
-console.log(lista)
+console.log(lista);
