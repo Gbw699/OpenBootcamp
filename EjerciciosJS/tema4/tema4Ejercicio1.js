e03216c522a7e1a60a30dd22d75718017e920be0
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-09-01 12:39:11 -0300

    aux

--- EjerciciosJS/tema4/tema4Ejercicio1.js
+++ EjerciciosJS/tema4/tema4Ejercicio1.js
@@ -1,29 +1,29 @@
 let nombre = "Gabriel";
-console.log(nombre)
+console.log(nombre);
 
 let apellido = "Barimboim";
-console.log(apellido)
+console.log(apellido);
 
 let estudiante = `${nombre} ${apellido}`;
-console.log(estudiante)
+console.log(estudiante);
 
 let estudianteMayus = estudiante.toUpperCase();
-console.log(estudianteMayus)
+console.log(estudianteMayus);
 
 let estudianteMinus = estudiante.toLowerCase();
-console.log(estudianteMinus)
+console.log(estudianteMinus);
 
 let num_letras = estudiante.length;
-console.log(num_letras)
+console.log(num_letras);
 
 let first_let = nombre[0];
-console.log(first_let)
+console.log(first_let);
 
-let last_let = apellido[apellido.length-1]
-console.log(last_let)
+let last_let = apellido[apellido.length - 1];
+console.log(last_let);
 
 let nonSpaceEstudiante = estudiante.replace(" ", "");
-console.log(nonSpaceEstudiante)
+console.log(nonSpaceEstudiante);
 
-let cont = estudiante.startsWith(nombre)
-console.log(cont)
+let cont = estudiante.startsWith(nombre);
+console.log(cont);
